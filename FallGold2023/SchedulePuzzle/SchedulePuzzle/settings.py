897f6a0517ae8de1a31fd99feacb4136b636b477
Ashley Carter <acart052@odu.edu>
2023-10-14 19:15:07 -0400

    swtiched to postgresql

--- FallGold2023/SchedulePuzzle/SchedulePuzzle/settings.py
+++ FallGold2023/SchedulePuzzle/SchedulePuzzle/settings.py
@@ -63,13 +63,24 @@ WSGI_APPLICATION = 'SchedulePuzzle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'schedulepuzzle',
+        'USER': 'gold',
+        'PASSWORD': 'gold411w',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
