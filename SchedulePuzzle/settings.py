271874349d95133b53367de5826db171dbfb67ea
Ashley Carter <acart052@odu.edu>
2023-10-21 14:43:08 -0400

    setting up postgresql

--- SchedulePuzzle/settings.py
+++ SchedulePuzzle/settings.py
@@ -72,11 +72,24 @@ WSGI_APPLICATION = 'SchedulePuzzle.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# # SQLite database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# PostgreSQL database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SchedulePuzzle',
+        'USER': 'gold',
+        'PASSWORD': '411wgold',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
