fdd60da4693d753e82b165a145e06b67c0ff2219
Ashley Carter <acart052@odu.edu>
2023-10-25 10:25:22 -0400

    created location table

--- AppCalendar/models.py
+++ AppCalendar/models.py
@@ -7,3 +7,9 @@ class Schedule(models.Model):
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     schedule_name = models.CharField(max_length=200)
     schedule_source = models.CharField(max_length=200)
+
+class Location(models.Model):
+    location_id = models.AutoField(primary_key=True)
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
