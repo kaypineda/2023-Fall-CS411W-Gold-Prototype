303b2242ad2994f2be606bda25bb29a061b8e14e
Ashley Carter <acart052@odu.edu>
2023-10-25 09:51:43 -0400

    created schedule model

--- AppCalendar/models.py
+++ AppCalendar/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from AppUser.models import AppUser
 
 # Create your models here.
+class Schedule(models.Model):
+    schedule_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    schedule_name = models.CharField(max_length=200)
+    schedule_source = models.CharField(max_length=200)
+    schedule_attributes = models.TextField()
