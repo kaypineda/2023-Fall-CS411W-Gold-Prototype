394d0a506756ac0e1438972067b04ef1e78c2b98
Ashley Carter <acart052@odu.edu>
2023-10-25 09:48:03 -0400

    added AppUser to installed apps

--- SchedulePuzzle/settings.py
+++ SchedulePuzzle/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AppUser',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SchedulePuzzle.wsgi.application'
 
+AUTH_USER_MODEL = 'AppUser.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
