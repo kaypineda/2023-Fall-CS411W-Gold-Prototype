ef2790715dc21b427cdae793802e5bd11b23bc42
Ashley Carter <acart052@odu.edu>
2023-10-30 14:18:21 -0400

    fixed url warning

--- AppCalendar/urls.py
+++ AppCalendar/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('calendar/', views.CalendarView.as_view(), name='calendar'),
     path('task/new/', views.task, name='task_new'),
-	path('task/edit/(?P<task_id>\d+)/', views.task, name='task_edit'),
+	path('task/edit/<int:task_id>/', views.task, name='task_edit'),
 ]
