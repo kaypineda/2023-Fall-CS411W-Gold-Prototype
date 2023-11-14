28c889052efc765ebdcbcd1d11118151bbfc6e10
kaypineda <kaylizp@gmail.com>
2023-11-13 20:25:57 -0500

    fixing merge errors

--- AppCalendar/models.py
+++ AppCalendar/models.py
@@ -30,7 +30,7 @@ class Task(models.Model):
 
 class Notifications(models.Model):
     notification_id = models.AutoField(primary_key=True)
-    event_id = models.ForeignKey('Event', on_delete=models.CASCADE)
+    task_id = models.ForeignKey('Task', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     notification_time=models.DateTimeField()
     TYPE_CHOICES = [
