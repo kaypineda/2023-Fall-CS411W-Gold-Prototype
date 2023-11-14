bfddb87a08856ad9bdff181e080acfff5e958acb
Ashley Carter <acart052@odu.edu>
2023-11-14 03:23:04 -0500

    moved export into own file

--- AppCalendar/views.py
+++ AppCalendar/views.py
@@ -63,52 +63,5 @@ def task(request, task_id=None):
         return HttpResponseRedirect(reverse('AppCalendar:calendar'))
     return render(request, 'AppCalendar/task.html', {'form': form})
 
-def export(request):
-    if request.method == 'POST':
-        format = request.POST.get('format')
 
-    if format == 'csv':
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="schedule.csv"'
-
-        writer = csv.writer(response)
-        writer.writerow(['Subject', 'Start Date', 'Start Time', 'End Date', 'End Time', 
-                         'All Day Event', 'Description', 'Location', 'Private'])
-        
-        for task in Task.objects.all().values_list('title', 'start_time', 'end_time','description'):
-            start_time = task[1].time()
-            end_time = task[2].time() 
-
-            all_day_event = 'True' if start_time == time(0,0) and end_time == time(23,59,59) else 'False'
-            writer.writerow([task[0], task[1].date(), start_time.strftime('%H:%M:%S'), task[2].date(), end_time.strftime('%H:%M:%S'),
-                             all_day_event, task[3], '', ''])
-        return response
-    
-    elif format == 'ics':
-        response = HttpResponse(content_type='text/calendar')
-        response['Content-Disposition'] = 'attachment; filename="schedule.ics"'
-        
-        cal = icalendar.Calendar()
-        cal.add('prodid', '-//Schedule Puzzle//EN')
-        cal.add('version', '2.0')
-             
-        tasks = Task.objects.all()
-        for task in tasks:
-            ical_task = icalendar.Event()
-           
-            ical_task.add('uid', task.task_id)
-            ical_task.add('summary', task.title)
-            ical_task.add('dtstart', task.start_time)
-            ical_task.add('dtend', task.end_time)
-            ical_task.add('dtstamp', datetime.now())
-            ical_task.add('description', task.description)
-
-            cal.add_component(ical_task)
-            print(f"Added event: {task.title}, Start: {task.start_time}, End: {task.end_time}")
-    
-        response.write(cal.to_ical())
-
-        return response
-
-    return redirect('AppCalendar:calendar')
 
