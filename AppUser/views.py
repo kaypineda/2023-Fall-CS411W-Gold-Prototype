b9fbe4ba91b54655203b88fba7c921bfd39724f7
Ashley Carter <acart052@odu.edu>
2023-11-27 13:45:46 -0500

    added dashboard view

--- AppUser/views.py
+++ AppUser/views.py
@@ -3,14 +3,19 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'AppUser/dashboard.html')
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        print(username, password)
         user = authenticate(request, username=username, password=password)
+        print(user)
         
         if user is not None:
-            user_login(request, user)
+            login(request, user)
             return redirect('AppUser:dashboard')
         else:
             messages.success(request, ('There was an error logging in. Please try again.'))
