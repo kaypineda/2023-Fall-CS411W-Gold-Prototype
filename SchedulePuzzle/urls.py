b107de8f82a47029709a8a22a78fa254274c81d8
Ashley Carter <acart052@odu.edu>
2023-11-27 09:51:54 -0500

    updated AppUser url

--- SchedulePuzzle/urls.py
+++ SchedulePuzzle/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include ('django.contrib.auth.urls')),
     path('', include('AppCalendar.urls')),
-    path('AppUser/', include('AppUser.urls')),
+    path('', include('AppUser.urls')),
 ]
