e033387908bafdc8d0508561ec73a992cbe0c0b9
Ashley Carter <acart052@odu.edu>
2023-11-28 13:37:19 -0500

    now shows user and users for admin site

--- AppUser/models.py
+++ AppUser/models.py
@@ -29,6 +29,10 @@ class EmailUser(AbstractBaseUser, PermissionsMixin):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
         
     def __str__(self):
         return self.email
