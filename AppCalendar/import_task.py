2a24fb8ac15a2e76ecb6f74ac6720728cb6c0b1b
Ashley Carter <acart052@odu.edu>
2023-11-28 15:42:34 -0500

    fixed indentation

--- AppCalendar/import_task.py
+++ AppCalendar/import_task.py
@@ -18,28 +18,28 @@ def read_csv(csv_file):
         end_time = row.get('End Time')
         description = row.get('Description')
         
-    start_datetime = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M:%S')
-    end_datetime = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M:%S')
+        start_datetime = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M:%S')
+        end_datetime = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M:%S')
     
-    formatted_start_time = start_datetime.strftime('%Y-%m-%dT%H:%M')
-    formatted_end_time = end_datetime.strftime('%Y-%m-%dT%H:%M')
+        formatted_start_time = start_datetime.strftime('%Y-%m-%dT%H:%M')
+        formatted_end_time = end_datetime.strftime('%Y-%m-%dT%H:%M')
     
-    existing_task = Task.objects.filter(
-        title = title, 
-        start_time = formatted_start_time, 
-        end_time = formatted_end_time
-        ).first()
-    
-    if existing_task:
-        dupe_task_list.append(existing_task)
-    else:
-        task_list.append(Task(
-            title = title,
-            start_time = formatted_start_time,
-            end_time = formatted_end_time,
-            description = description
-        ))
+        existing_task = Task.objects.filter(
+            title = title, 
+            start_time = formatted_start_time, 
+            end_time = formatted_end_time
+            ).first()
         
-        return task_list, dupe_task_list
+        if existing_task:
+            dupe_task_list.append(existing_task)
+        else:
+            task_list.append(Task(
+                title = title,
+                start_time = formatted_start_time,
+                end_time = formatted_end_time,
+                description = description
+            ))
+            
+    return task_list, dupe_task_list
         
             
