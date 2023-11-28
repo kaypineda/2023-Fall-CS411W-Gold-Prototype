2ded60f5549b6297907d9bfeb84f298139912394
Ashley Carter <acart052@odu.edu>
2023-11-28 15:51:43 -0500

    added import url

--- AppCalendar/urls.py
+++ AppCalendar/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from . import export_task
+from . import export_task, import_task
 
 app_name = 'AppCalendar'
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
 	path('task/edit/<int:task_id>/', views.task, name='task_edit'),
     path('task/delete/<int:task_id>/', views.task_delete, name='task_delete'),
     path('export/', export_task.export, name='export'),
+    path('import/', import_task.import_csv, name='import'),
 ]
