ccba12d73841e08da96c18202509d2399ca91905
Ashley Carter <acart052@odu.edu>
2023-11-28 12:18:44 -0500

    created cusom super user

--- AppUser/models.py
+++ AppUser/models.py
@@ -11,4 +11,11 @@ class CustomUserManager(BaseUserManager):
         user = self.model(email = email, **extra_fields)
         user.set_password(password)
         user.save()
+        
         return user
+    
+    def create_superuser(self, email, password = None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        return self.create_user(email, password, **extra_fields)
