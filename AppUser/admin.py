eb7d952e80590660f708bc4688c12905c729cb6e
Ashley Carter <acart052@odu.edu>
2023-11-28 17:00:01 -0500

    removed double import from merge

--- AppUser/admin.py
+++ AppUser/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-
-from .models import CustomUser
-
-admin.site.register(CustomUser)
-# Register your models here.
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import EmailUser 
 
