1cfb60889d89a903218ac329c6231f483289bab9
Ashley Carter <acart052@odu.edu>
2023-11-28 12:13:49 -0500

    creating default custom user

--- AppUser/models.py
+++ AppUser/models.py
@@ -2,4 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
-
+class CustomUserManager(BaseUserManager):
+    
+    def create_user(self, email, password = None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
