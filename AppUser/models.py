59c7c204eb877b9b1c7d1f46fd2096b61279d964
Ashley Carter <acart052@odu.edu>
2023-11-28 02:38:49 -0500

    removed phone number

--- AppUser/models.py
+++ AppUser/models.py
@@ -4,4 +4,3 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15)
