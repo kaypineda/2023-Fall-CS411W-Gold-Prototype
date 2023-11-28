1d661d205e59bb9c0c19b5698f89f2e3c7bc7a56
Ashley Carter <acart052@odu.edu>
2023-11-28 12:20:38 -0500

    added email class

--- AppUser/models.py
+++ AppUser/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
@@ -19,3 +19,16 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         
         return self.create_user(email, password, **extra_fields)
+
+class EmailUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+        
+    def __str__(self):
+        return self.email
