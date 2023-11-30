43b7f109d78d6e1c0946874ccc01b2025759a9ed
kaypineda <kaylizp@gmail.com>
2023-11-29 21:41:57 -0500

    (in progress) making css available to all apps

--- SchedulePuzzle/settings.py
+++ SchedulePuzzle/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    #BASE_DIR / "static",
+    BASE_DIR / "SchedulePuzzle/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
