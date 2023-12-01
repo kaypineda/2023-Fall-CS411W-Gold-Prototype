d9bdfa8e67f570957fb1bbaebe8254a92bc77dea
Ashley Carter <acart052@odu.edu>
2023-12-01 14:26:29 -0500

    added register url

--- AppUser/urls.py
+++ AppUser/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'AppUser'
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('login/', views.user_login, name='user_login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.user_register, name='user_register'),
 ]
