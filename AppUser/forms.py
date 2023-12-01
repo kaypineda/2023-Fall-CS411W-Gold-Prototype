a70d484567acfd5cf389ce6d459a8fefbb051407
Ashley Carter <acart052@odu.edu>
2023-12-01 14:26:23 -0500

    using django register form

---
+++ AppUser/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class CustomUserForm(UserCreationForm):
+    email = forms.EmailField(required = True)
+    
+    class Meta:
+        model = get_user_model()
+        fields = ("email", "password1", "password2")
+        
+    def save_user(self, commit = True):
+        user = super().save(commit = False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user 
