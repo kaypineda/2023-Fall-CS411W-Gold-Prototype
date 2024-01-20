1213fb7cd538fd61a9466e275beda87ec3fc451b
Jake R. Austin <89155398+jaust6@users.noreply.github.com>
2024-01-20 11:53:04 +0900

    Update admin.py
    adding address and get_weather to class Task, also restructure the code of the file.

--- AppCalendar/admin.py
+++ AppCalendar/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from .models import Schedule, Task
 
 # Register your models here.
-admin.site.register(Task)
-admin.site.register(Schedule)
+
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['task_id', 'category', 'priority', 'title', 'description', 'start_time', 'end_time','address','weather']
+    list_display = ['task_id', 'category', 'priority', 'title', 'description', 'start_time', 'end_time','address','get_weather']
+    
+ #def get_weather(self, obj):
+        #return obj.weather if obj.weather else "Weather information not available"
+
+    #get_weather.short_description = 'Weather'
+
+admin.site.register(Task,TaskAdmin)
+admin.site.register(Schedule)
