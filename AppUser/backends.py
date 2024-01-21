0dd65a2e309d9c1ed7211465c04e93b0b5d1bb6e
Jake R. Austin <89155398+jaust6@users.noreply.github.com>
2024-01-21 17:46:55 +0900

    Update backends.py
    Better handling case sensitivity and easy code readability.

--- AppUser/backends.py
+++ AppUser/backends.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from typing import Optional # For better code readability and maintainability.
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
+    def authenticate(self, request, username: str = None, password: str = None, **kwargs) -> Optional[get_user_model()]:
         
         """
         Authenticates a user by email and password.
@@ -19,13 +20,26 @@ class EmailBackend(ModelBackend):
         
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(email=username)
+            user = UserModel.objects.get(email__iexact=username)
             if user.check_password(password):
                 return user
         except UserModel.DoesNotExist:
+            logger.warning(f"Authentication failed for email: {username}")
+            return None
+        except Exception as e:
+            logger.error(f"Authentication error: {e}")
             return None
 
-    def get_user(self, user_id):
+    def get_user(self, user_id)-> Optional[get_user_model()]:
+        """
+        Retrieve a user by user ID.
+
+        Parameters:
+            user_id (int): The user ID.
+
+        Returns:
+            get_user_model(): The user instance if found, None otherwise.
+        """
         UserModel = get_user_model()
         try:
             return UserModel.objects.get(pk=user_id)
